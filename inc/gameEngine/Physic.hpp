aa658adc5cf918b9585ce29b36cf2fff3486990a
Thomas Fossaert <thomas.fossaert@epitech.eu>
2018-01-10 15:54:38 +0100

    [+] [GameEngine] Physic

---
+++ inc/gameEngine/Physic.hpp
@@ -0,0 +1,19 @@
+//
+// Created by thomas on 10/01/18.
+//
+
+#ifndef CPP_RTYPE_PHYSIC_HPP
+#define CPP_RTYPE_PHYSIC_HPP
+
+class Physic
+{
+  public:
+    Physic();
+    ~Physic();
+
+  private:
+    int _gravity;
+    unsigned int _speed;
+};
+
+#endif //CPP_RTYPE_PHYSIC_HPP
