baf78e826bf77e9d72835d3af6e5d45ff4300ebb
Thomas Fossaert <thomas.fossaert@epitech.eu>
2018-01-15 13:23:30 +0100

    [*] [Game/Class] Trying joystick

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -9,9 +9,60 @@
 
 int main()
 {
-  Player p;
-  Ennemy e;
+  Position2D pos(200, 200);
+  ColliderManager collider;
+  Window window("hey", 800, 800);
+  Sprite sprite("../Sprite-Logo.jpg", pos);
 
-  std::cout << p.getHp() << std::endl;
-  std::cout << e.getHp() << std::endl;
+  while (window.isOpen())
+  {
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+
+      if (sf::Joystick::isConnected(0))
+      {
+	if (sf::Joystick::isButtonPressed(0, 1))
+	{
+	  std::cout << "FIRE" << std::endl;
+	}
+	float x = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+	float y = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+	sprite.move(x/5, y/5);
+      }
+      else
+      {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+	  sprite.move(-10, 0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+	  sprite.move(0, -10);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+	  sprite.move(0, 10);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+	  sprite.move(10, 0);
+	}
+      }
+      switch (event.type)
+      {
+	case sf::Event::Closed:
+	  window.close();
+
+	  break;
+
+      }
+    }
+
+
+    window.clear();
+    window.draw(sprite);
+    window.display();
+  }
 }
