c7718669915951ba14f7a207053e877989e38898
Unknown <adrien.warin@epitech.eu>
2018-01-18 14:12:46 +0100

    [*] [Network] RE: fix command Manager
    use for auto & remove comment

--- src/server/rtype/CommandManager.cpp
+++ src/server/rtype/CommandManager.cpp
@@ -20,17 +20,19 @@ void CommandManager::executeCommand(std::string funcName, std::vector<Room> &roo
 void CommandManager::getRooms(const std::vector<Room> &rooms)
 {
     std::cout << "List of Rooms:" << std::endl;
-    for (size_t n = 0; n < rooms.size(); n++)
-        std::cout << rooms[n].getId() << std::endl;
+    for (auto &it : rooms)
+    {
+        std::cout << "Room id :" << it.getId() << std::endl;
+    }
 }
 
 void CommandManager::getPlayers(const std::vector<Room>& rooms)
 {
-    for (size_t n = 0; n < rooms.size(); n++) {
-        std::cout << "Room's name : " << rooms[n].getId() << std::endl;
-/*        for (size_t i = 0; i < rooms[n].getPlayer().size(); i++)
-            std::cout << rooms[n].getPlayer()[i] << std::endl;
-TODO we need a getPlayer() method in room
-*/
+    for (auto &it : rooms)
+    {
+        std::cout << "ROOM id: " << it.getId() << std::endl;
+        for (auto &it2 : it.getPlayers()) {
+            std::cout << it2.getName() << std::endl;
+        }
     }
 }
